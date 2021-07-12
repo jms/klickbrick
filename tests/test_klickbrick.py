73e907c7bdb3982083974b10a97988818ec8a2ef
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2021-07-12 08:51:28 -0600

    update test

--- tests/test_klickbrick.py
+++ tests/test_klickbrick.py
@@ -1,9 +1,9 @@
 import pytest
-from klickbrick import __version__, Klickbrick
+import klickbrick
 
 
 def test_version():
-    assert __version__ == '0.1.1'
+    assert klickbrick.__version__ == '0.1.1'
 
 
 @pytest.mark.parametrize("args, response", [
@@ -12,11 +12,11 @@ def test_version():
     (["hello", "-n", "Joe"], "Hello Joe\n"),
 ])
 def test_cli_default(args, response, capsys):
-    Klickbrick(args)
+    klickbrick.Klickbrick(args)
     captured = capsys.readouterr()
     assert captured.out == response
 
 
 def test_cli_no_option():
     with pytest.raises(SystemExit):
-        Klickbrick([])
+        klickbrick.Klickbrick([])
