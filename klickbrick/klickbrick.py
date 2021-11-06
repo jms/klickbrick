af2ae2c6692f91639dd6f9cf67fac8acaa48e115
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2021-11-06 11:19:38 -0600

    add test

--- klickbrick/klickbrick.py
+++ klickbrick/klickbrick.py
@@ -9,17 +9,43 @@ class Klickbrick:
     def __init__(self, arguments: List[Any]):
         self.parser = argparse.ArgumentParser(prog="klickbrick", usage="%(prog)s [options]")
         self.subparsers = self.parser.add_subparsers(help="sub-command help")
+
         self.subparsers.dest = "hello"
         self.subparsers.required = True
 
         hello_parser = self.subparsers.add_parser(name="hello", description="A friendly Hello World")
         hello_parser.add_argument("-n", "--name", type=str, default="World")
 
+        toolchain_parser = self.subparsers.add_parser(name="onboard", description="Toolchain commands")
+
+        toolchain_parser.add_argument("--dry-run", type=str)
+        toolchain_parser.add_argument("--name", type=str, required=True)
+        toolchain_parser.add_argument("--email", type=str, required=True)
+        toolchain_parser.add_argument("--commit-template", type=str)
+
         options = self.parser.parse_args(arguments)  # type: argparse.Namespace
         getattr(self, arguments[0])(options.name)
 
-    def hello(self, name: str):
+    @staticmethod
+    def hello(name: str):
         print(greeting(name))
+
+    @staticmethod
+    def onboard(name: str, email: str):
+        """
+        git tasks:
+
+        git config --global user.name "John Doe"
+        git config --global user.email johndoe@example.com
+        git config --global init.defaultBranch main
+        git config --global commit.template ~/.gitmessage.txt
+
+        :param name:
+        :param email:
+        :return:
+        """
+        commit_template: str = "~/.gitmessage.txt"
+        pass
 
 
 def main():
--- klickbrick/script.py
+++ klickbrick/script.py
@@ -1,2 +1,6 @@
 def greeting(name):
     return f"Hello {name}"
+
+
+def onboard():
+    pass
